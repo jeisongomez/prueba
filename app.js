18d54394492fd9e3b7f89141b9b2f4661227a83a
jeisongomez <jeison_gomez1997@hotmail.com>
2018-03-15 19:05:12 -0500

    prueba de consumo json

---
+++ app.js
@@ -0,0 +1,10 @@
+
+angular.module("MyfirstApp",[]).controller("FirstController", function($scope, $http){
+		$scope.posts = [];
+		$http.get("http://jsonplaceholder.typicode.com/posts").success(function(data){
+				console.log(data);
+				$scope.posts = data;
+			}).error(function(err){
+
+			});
+	});
